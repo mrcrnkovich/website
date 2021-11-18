65fbe9d7435cd173ada87d8ac786d821656a36d8
mrcrnkovich <mrcrnkovich@gmail.com>
2021-11-17 19:06:46 -0500

    moved template components into base folder

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def base_route():
-        return "this is a test"
+    from . import views
+    app.register_blueprint(views.bp)
 
     return app
